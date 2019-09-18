246e4450d9f219a50086b0e27b2a1b5b75e1b88d
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-18 16:16:45 +0430

    develop of Back

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Pishkhan.Data;
+using Pishkhan.Models;
+
+namespace Pishkhan.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppIdentityUser> signInManager;
+        private readonly UserManager<AppIdentityUser> userManager;
+        private readonly JwtTokenModel jwtTokenModel;
+        public AccountController(SignInManager<AppIdentityUser> signInManager,
+            UserManager<AppIdentityUser> userManager,
+            IOptions<JwtTokenModel> options)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            jwtTokenModel = options.Value;
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody]Models.LoginModel loginModel)
+        {
+            if (!ModelState.IsValid) return StatusCode(406);
+            var result = await signInManager.PasswordSignInAsync(loginModel.UserName, loginModel.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                var appUser = await userManager.FindByNameAsync(loginModel.UserName);
+                return Ok(GenerateJwtToken(appUser));
+            }
+
+            return NotFound();
+        }
+
+
+        private string GenerateJwtToken(IdentityUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtTokenModel.Key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(30));
+
+            var token = new JwtSecurityToken(
+                jwtTokenModel.Issuer,
+                jwtTokenModel.Audience,
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+}
--- Startup.cs
+++ Startup.cs
@@ -1,25 +1,76 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Pishkhan.Data;
+using Pishkhan.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace Pishkhan
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfigurationRoot Configuration;
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var jwtTokenModel = new JwtTokenModel();
+
+            Configuration.GetSection("JwtToken").Bind(jwtTokenModel);
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+       options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+                    .AddEntityFrameworkStores<AppIdentityDbContext>()
+                    .AddDefaultTokenProviders();
+
+            // ===== Add Jwt Authentication ========
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = jwtTokenModel.Issuer,
+                        ValidAudience = jwtTokenModel.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtTokenModel.Key)),
+                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                    };
+                });
+
+
+            services.Configure<JwtTokenModel>(config => Configuration.GetSection("JwtToken").Bind(config));
+
+            services.AddMvc();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
@@ -45,6 +96,8 @@ namespace Pishkhan
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
