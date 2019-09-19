932f9711db194c7a938a26ae72dea3085b5839d5
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-09-19 12:56:01 +0430

    component menu

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ColorLine from './ColorLine';
 import Header from './Header';
+import Menu from './Menu';
 
 const Home = props => (
   <div>
     <ColorLine />
     <Header />
+    <Menu />
   </div>
 );
 
