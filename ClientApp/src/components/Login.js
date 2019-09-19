33c0c3fb7dfe2f2740a248dfa96df5bda1c3bee2
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-09-19 12:26:36 +0430

    component login and register hooks

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,39 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
-export default class Login extends Component {
-    render() {
-        return (
-            <div className="login">
-                <div className="col-md-4 col-sm-6 col-xs-12">
-                    <div className="login__box">
-                        <form>
-                            <div className="text-center">
-                                <div className="login__box__icon">
-                                    <i className="fa fa-sign-in"></i>
-                                </div>
+export default function Login() {
+    return (
+        <div className="login">
+            <div className="col-md-4 col-sm-6 col-xs-12">
+                <div className="login__box">
+                    <form>
+                        <div className="text-center">
+                            <div className="login__box__icon">
+                                <i className="fa fa-sign-in"></i>
                             </div>
-                            <div className="login__box__title">ورود به سایت </div>
-                            <div className="form-group">
-                                <label>نام کاربری را وارد کنید </label>
-                                <input type="text" tabIndex="1" className="form-control" placeholder="نام کاربری" />
-                            </div>
-                            <div className="form-group">
-                                <label>رمز عبور را وارد کنید </label>
-                                <input type="password" tabIndex="2" className="form-control" placeholder="رمز عبور" />
-                            </div>
-                            <div className="login__box__link">
-                                <Link to="/register">ثبت نام</Link>
-                                <Link to="/register">فراموشی رمز عبور</Link>
-                                {/* <a href="#">فراموشی رمز عبور</a> */}
-                            </div>
-                            <div className="form-group">
-                                <button tabIndex="3" type="submit" className="btn btn-success btn-block">ورود</button>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div className="login__box__title">ورود به سایت </div>
+                        <div className="form-group">
+                            <label>نام کاربری را وارد کنید </label>
+                            <input type="text" tabIndex="1" className="form-control" placeholder="نام کاربری" />
+                        </div>
+                        <div className="form-group">
+                            <label>رمز عبور را وارد کنید </label>
+                            <input type="password" tabIndex="2" className="form-control" placeholder="رمز عبور" />
+                        </div>
+                        <div className="login__box__link">
+                            <Link to="/register">ثبت نام</Link>
+                            <Link to="/register">فراموشی رمز عبور</Link>
+                            {/* <a href="#">فراموشی رمز عبور</a> */}
+                        </div>
+                        <div className="form-group">
+                            <button tabIndex="3" type="submit" className="btn btn-success btn-block">ورود</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
--- ClientApp/src/components/Register.js
+++ ClientApp/src/components/Register.js
@@ -1,41 +1,39 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-export default class Register extends Component {
-    render() {
-        return (
-            <div className="login">
-                <div className="col-md-4 col-sm-6 col-xs-12">
-                    <div className="login__box">
-                        <form>
-                            <div className="text-center">
-                                <div className="login__box__icon">
-                                    <i className="fa fa-user"></i>
-                                </div>
+export default function Register() {
+    return (
+        <div className="login">
+            <div className="col-md-4 col-sm-6 col-xs-12">
+                <div className="login__box">
+                    <form>
+                        <div className="text-center">
+                            <div className="login__box__icon">
+                                <i className="fa fa-user"></i>
                             </div>
-                            <div className="login__box__title">ثبت نام در سایت </div>
-                            <div className="form-group">
-                                <label>نام و نام خانوادگی را وارد کنید </label>
-                                <input type="text" tabIndex="1" className="form-control" placeholder="نام و نام خانوادگی" />
-                            </div>
-                            <div className="form-group">
-                                <label>ایمیل را وارد کنید </label>
-                                <input type="email" tabIndex="2" className="form-control" placeholder="ایمیل" />
-                            </div>
-                            <div className="form-group">
-                                <label>شماره تلفن را وارد کنید </label>
-                                <input type="email" tabIndex="3" className="form-control" placeholder="شماره تلفن" />
-                            </div>
-                            <div className="login__box__link">
-                                <Link to="/login">ورود به سایت </Link>
-                            </div>
-                            <div className="form-group">
-                                <button tabIndex="4" type="submit" className="btn btn-success btn-block">ثبت نام</button>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div className="login__box__title">ثبت نام در سایت </div>
+                        <div className="form-group">
+                            <label>نام و نام خانوادگی را وارد کنید </label>
+                            <input type="text" tabIndex="1" className="form-control" placeholder="نام و نام خانوادگی" />
+                        </div>
+                        <div className="form-group">
+                            <label>ایمیل را وارد کنید </label>
+                            <input type="email" tabIndex="2" className="form-control" placeholder="ایمیل" />
+                        </div>
+                        <div className="form-group">
+                            <label>شماره تلفن را وارد کنید </label>
+                            <input type="email" tabIndex="3" className="form-control" placeholder="شماره تلفن" />
+                        </div>
+                        <div className="login__box__link">
+                            <Link to="/login">ورود به سایت </Link>
+                        </div>
+                        <div className="form-group">
+                            <button tabIndex="4" type="submit" className="btn btn-success btn-block">ثبت نام</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
