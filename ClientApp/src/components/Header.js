46db6d67519d2244b9da7a20097b587bde8e3c4c
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-09-20 15:17:39 +0430

    logOut

--- ClientApp/src/components/Header.js
+++ ClientApp/src/components/Header.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom';
 
 export default function Header() {
+    const [isLogin, setIsLogin] = useState(true)
     const sidebar = () => {
-        
+
         const sidebar = document.body.classList.contains('hide-sidebar');
         const pagesmall = document.body.classList.contains('page-small');
 
@@ -21,18 +23,28 @@ export default function Header() {
             }
         }
     }
+
+    const logOut = () => {
+        localStorage.removeItem('token');
+        setIsLogin(false)
+    }
     return (
-        <header className="header">
-            <div className="header__welcome">به ناحیه کاربری خودتون خوش آمدید</div>
-            <div className="header__dashbord" onClick={sidebar}>
-                <i className="fa fa-bars"></i>
-            </div>
-            <a className="header__link" data-toggle="tooltip" data-placement="top" title="خروج از سامانه">
-                <i className="fa fa-sign-in"></i>
-            </a>
-            <a className="header__link" data-toggle="tooltip" data-placement="top" title="خروج از سامانه">
-                <i className="fa fa-comment-o"></i>
-            </a>
-        </header>
+        <div>
+            {
+                isLogin ? <header className="header">
+                    < div className="header__welcome" > به ناحیه کاربری خودتون خوش آمدید</div >
+                    <div className="header__dashbord" onClick={sidebar}>
+                        <i className="fa fa-bars"></i>
+                    </div>
+                    <a onClick={logOut} className="header__link" data-toggle="tooltip" data-placement="top" title="خروج از سامانه">
+                        <i className="fa fa-sign-in"></i>
+                    </a>
+                    <a className="header__link" data-toggle="tooltip" data-placement="top" title="خروج از سامانه">
+                        <i className="fa fa-comment-o"></i>
+                    </a>
+                </header >
+                    : <Redirect to="/" />
+            }
+        </div>
     )
 }
