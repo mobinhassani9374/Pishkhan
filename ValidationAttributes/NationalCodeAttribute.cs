d2b0b6fd5a7958b03a7fb1aa52769b98f9298d23
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-20 17:22:27 +0430

    develop of register

---
+++ ValidationAttributes/NationalCodeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using DNTPersianUtils.Core;
+
+namespace Pishkhan.ValidationAttributes
+{
+    public class NationalCodeAttribute:ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null) return false;
+
+            var nationalCode = (string)value;
+
+            return nationalCode.IsValidIranianNationalCode();
+        }
+    }
+}
