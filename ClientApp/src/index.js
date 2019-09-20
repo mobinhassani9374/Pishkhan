cd42b21d8a2a7b4ee043b13b1a7bab72f94fa5d7
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-09-20 12:33:19 +0430

    add toaster

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -9,6 +9,7 @@ import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ToastProvider } from 'react-toast-notifications'
 
 // Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
@@ -23,7 +24,9 @@ const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <ToastProvider autoDismissTimeout={5000} placement ={'bottom-left'}>
+        <App />
+      </ToastProvider>
     </ConnectedRouter>
   </Provider>,
   rootElement);
