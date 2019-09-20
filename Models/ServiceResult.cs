793526a355828e28ddea375e4e6526ae3e8e6387
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-20 12:19:15 +0430

    add ServiceResult

---
+++ Models/ServiceResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pishkhan.Models
+{
+    public class ServiceResult
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public List<string> Errors { get; set; }
+    }
+
+    public class ServiceResult<T> : ServiceResult
+    {
+        public T Data { get; set; }
+    }
+
+}
