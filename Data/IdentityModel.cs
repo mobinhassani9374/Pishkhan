585994e29f6523227dcc50bfa1db91b013e95e82
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-20 13:38:46 +0430

    AddEntity_UserPhoneNumber_Migration

--- Data/IdentityModel.cs
+++ Data/IdentityModel.cs
@@ -14,6 +14,19 @@ namespace Pishkhan.Data
     public class AppIdentityUser : IdentityUser
     {
         public string NationalCode { get; set; }
+
+        public virtual ICollection<UserPhoneNumber> PhoneNumbers { get; set; }
+    }
+
+    public class UserPhoneNumber
+    {
+        public int Id { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public virtual AppIdentityUser User { get; set; }
+
+        public string UserId { get; set; }
     }
 
     public class AppIdentityDbContext
@@ -22,6 +35,8 @@ namespace Pishkhan.Data
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
             : base(options)
         { }
+
+        public DbSet<UserPhoneNumber> UserPhoneNumbers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -32,7 +47,21 @@ namespace Pishkhan.Data
 
             user.Property(c => c.NationalCode).HasMaxLength(256);
             user.HasIndex(c => c.NationalCode).IsUnique(true);
-            
+
+            //userPhoneNumbers
+            var userPhoneNumber = builder.Entity<UserPhoneNumber>();
+
+            userPhoneNumber.Property(c => c.PhoneNumber).HasMaxLength(265).IsRequired(true);
+            userPhoneNumber.HasIndex(c => c.PhoneNumber).IsUnique(true);
+
+            userPhoneNumber.Property(c => c.UserId).HasMaxLength(450);
+
+            userPhoneNumber
+                .HasOne(c => c.User)
+                .WithMany(c => c.PhoneNumbers)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
         }
     }
 }
