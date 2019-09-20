cda58489a5e09d6fde4df6f825ca889a9057cb85
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-20 15:06:37 +0430

    add RegisterModel

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pishkhan.Models
+{
+    public class RegisterModel
+    {
+        public string NationalCode { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
