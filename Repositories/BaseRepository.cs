c35efb84cc1acbbd4872b36b97aba60d141da1ad
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-21 10:32:58 +0430

    add UserPhoneNumberRepository

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using Pishkhan.Data;
+using Pishkhan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pishkhan.Repositories
+{
+    public class BaseRepository<TEntity> where TEntity : class
+    {
+        protected readonly AppIdentityDbContext _context;
+        public BaseRepository(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public ServiceResult Create(TEntity entity)
+        {
+            _context.Add(entity);
+            return Save():
+        }
+        private ServiceResult Save()
+        {
+            if (_context.SaveChanges() > 0)
+                return ServiceResult.Okay();
+            return ServiceResult.Error();
+        }
+    }
+}
