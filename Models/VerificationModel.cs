f58a8e3aa98183a332064b30b8d562a6bad90d9b
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-21 15:02:57 +0430

    add VerificationModel

---
+++ Models/VerificationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pishkhan.Models
+{
+    public class VerificationModel
+    {
+        [Required(ErrorMessage = "شماره همراه نمی تواند فاقد مقدار باشد")]
+        public string PhoneNumber { get; set; }
+
+        public int ActivationCode { get; set; }
+    }
+}
