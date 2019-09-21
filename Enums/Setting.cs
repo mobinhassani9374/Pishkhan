f5bff76c4b779eb34a3a8ef12c0a86b62a974f03
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-21 14:51:04 +0430

    add Setting enum

---
+++ Enums/Setting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pishkhan.Enums
+{
+    public enum Setting
+    {
+        VerificationTime = 1
+    }
+}
