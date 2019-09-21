47bdc7495bb6317bfb9a02d68cc572f6be603da7
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-21 14:47:13 +0430

    add SettingRepository

---
+++ Repositories/SettingRepository.cs
@@ -0,0 +1,16 @@
+﻿using Pishkhan.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pishkhan.Repositories
+{
+    public class SettingRepository : BaseRepository<Setting>
+    {
+        public SettingRepository(AppIdentityDbContext context) : base(context)
+        {
+
+        }
+    }
+}
