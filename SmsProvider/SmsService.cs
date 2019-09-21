601e003587b0ab220be9d9453d5a3366d43e7b68
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-09-21 09:40:42 +0430

    add SmsService

---
+++ SmsProvider/SmsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pishkhan.SmsProvider
+{
+    public class SmsService
+    {
+        public void Send(string phoneNumber,string message)
+        {
+            // code send sms here
+        }
+    }
+}
