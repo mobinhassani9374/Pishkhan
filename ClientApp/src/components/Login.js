0f530fd821c68b2d351452ab77d8d939628aaa94
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-09-21 09:24:36 +0430

    add locale

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -4,6 +4,7 @@ import { Captcha, captchaSettings } from 'reactjs-captcha';
 import axios from 'axios';
 import { ToastsStore } from 'react-toasts';
 import LaddaButton, { XS, SLIDE_UP } from 'react-ladda';
+import { useTranslation } from 'react-i18next';
 
 export default function Login() {
 
@@ -20,6 +21,9 @@ export default function Login() {
     const [isLogin, setIsLogin] = useState(false)
     const [loading, setLoading] = useState(false)
     const captcha = useRef();
+    const { t, i18n } = useTranslation(['home']);
+
+    //i18n.changeLanguage('fa_IR');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -81,7 +85,7 @@ export default function Login() {
                                         <i className="fa fa-sign-in"></i>
                                     </div>
                                 </div>
-                                <div className="login__box__title">ورود به سایت </div>
+                                <div className="login__box__title">{t('login')}</div>
                                 <div className="form-group">
                                     <label>نام کاربری را وارد کنید </label>
                                     <input type="text" name="userName" required onChange={changeInputs} tabIndex="1" className="form-control" placeholder="نام کاربری" />
---
+++ ClientApp/src/locale/home.locale.js
@@ -0,0 +1,8 @@
+export default {
+    fa_IR: {
+      home: {
+        'login': 'ورود به سایت ',
+      }
+    },
+    // add other language to the object
+  };
